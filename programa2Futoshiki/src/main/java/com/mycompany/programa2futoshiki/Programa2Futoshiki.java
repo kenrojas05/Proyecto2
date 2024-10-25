ffd536352dcb21137a3c96b0b1890cc30396f6cb
kenrojas05 <kenrojas@estudiantec.cr>
2024-10-25 01:12:12 -0600

    Initial commit

---
+++ programa2Futoshiki/src/main/java/com/mycompany/programa2futoshiki/Programa2Futoshiki.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa2futoshiki;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Programa2Futoshiki {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
