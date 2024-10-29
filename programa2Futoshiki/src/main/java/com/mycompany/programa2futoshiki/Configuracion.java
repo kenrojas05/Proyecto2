c1e162a5cebd2561d8a6fcbc99b7e2f5f4bc1d03
kenrojas05 <kenrojas@estudiantec.cr>
2024-10-28 21:44:26 -0600

    Mini avance Pom y configuracion
    Dado que este proyecto decidi mejor usar maven y no ant entonces use el xml pom para agregar la dependency de javax.mail para el envio de correo del jugador y se creo la clase configuracion para guardar la configuracion basicamente

---
+++ programa2Futoshiki/src/main/java/com/mycompany/programa2futoshiki/Configuracion.java
@@ -0,0 +1,42 @@
+
+package com.mycompany.programa2futoshiki; //correo jvim woqi nqnz puyb
+
+//(Documentar mejor en el PDF al final)
+import java.util.HashMap;  //Map es un objeto
+import java.util.Map;      //lo que hace es que guarda valores con Keys o llaves (parecido a un directorio)
+
+import java.time.LocalDateTime; // Sirve para dar la hora del sistema
+import javax.mail.*; // Se añadió la librería llib al proyecto para la API javax.mail
+import java.util.Properties; 
+import javax.mail.internet.*; // Importa las clases necesarias para MIME
+
+
+/**
+ *
+ * @author Kendall Rojas Cartin
+ */
+public class Configuracion {
+    private byte cuadricula; //puesto que es cuadrada es decir 2x2 3x3 y asi solo ocupa el digito
+    private String nivel; //Facil, Intermedio, Dificil
+    private boolean multinivel; //false: no, true: si
+    private String reloj; //Cronometro, No, Temporizador
+    private boolean posicion; // false: derecha, true: izquierda
+
+    private Map<String, String> jugadores = new HashMap<>(); //nombre key password value
+    
+    private void setJugadores(String nombre, String password){
+        if (nombre.length()==0){
+            throw new IllegalArgumentException("INCOGNITO DEBERIA SER");
+        }
+        if (nombre.length()>30){
+            throw new IllegalArgumentException("Exceso de caracteres en el nombre (+30)");
+        }
+        else if (jugadores.containsKey(nombre)){
+            throw new IllegalArgumentException("Nombre debe ser unico");
+        }
+        else{
+            jugadores.put(nombre, password);
+        }
+        
+    }
+}
